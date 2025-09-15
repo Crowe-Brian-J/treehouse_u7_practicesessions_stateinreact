361d463c346ed83b629e56ada12ba2422e3fa580
Brian Crowe <brian.j.crowe@protonmail.com>
2025-09-15 11:50:42 -0400

    stub up StarRating.js and implement Star.js

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react'
+
 const StarRating = () => {
-    // Initialize a 'courseRating' state
+  // Initialize a 'courseRating' state
+  const [courseRating, setCourseRating] = useState(0)
 
+  // Write a function that returns 5 Star components
+  const renderStars = () => {
+    const stars = []
+    for (let i = 0; i < 5; i++) {
+      stars.push(
+        <li key={i} onClick={() => handleRating(i + 1)}>
+          {i < courseRating ? 'Yes' : 'No'}
+        </li>
+      )
+    }
+    return stars
+  }
 
-    // Write a function that returns 5 Star components
+  // Write an event handler that updates the courseRating state.
+  // Pass the function to a Star component via props
+  const handleRating = (rating) => {
+    setCourseRating(rating)
+  }
 
-
-    // Write an event handler that updates the courseRating state.
-    // Pass the function to a Star component via props
-    
-
-    return (
-        <ul className='course--stars'>
-            {/* Render the Star components */}
-        </ul>
-    );
+  return <ul className="course--stars">{renderStars}</ul>
 }
 
-export default StarRating;
+export default StarRating
